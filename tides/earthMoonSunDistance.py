dcfc25aa0d292dc8c2f92c9b04ad29d75f88a969
juhipark <juhipark@student.hartnell.edu>
2017-11-01 21:07:08 -0700

    Binded slider event with moon's postion

--- tides/earthMoonSunDistance.py
+++ tides/earthMoonSunDistance.py
@@ -5,12 +5,12 @@ import wx
 
 L=400
 Hgraph=400
-#Create a window.
-w = window(width=2*(L+window.dwidth), height=L+window.dheight+window.menuheight+Hgraph,
-           menus=True, title='The Earth\'s Tide', style=wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
+####Create a window.
+w = window(width=2*L, height=L+window.dheight+window.menuheight+Hgraph,
+           menus=True, title='Tide Change Simulation', style=wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
 
-#Placing display
-d=20
+####Placing display
+d=20#margin
 Hdisp=200
 disp = display(window=w, x=d, y=d, width=L*2-(d*2), height=Hdisp, forward=-vector(0,1,2), background=color.white)
 #########gdisplay(window=w, y=disp.height+50, width=2*(L+window.dwidth), height=Hgraph)
@@ -42,17 +42,28 @@ earth = sphere(material=materials.BlueMarble, radius=earthRad*2)
 moon = sphere(color=color.gray(2), material=materials.shiny, radius=moonRad*2)
 sun = sphere(pos=vector((sunDis/2),0,0), color=color.yellow, material=materials.emissive, radius=sunRad*2)
 
-#starting position
+####starting position
 earth.pos=vector(-(sunDis/2)+earthRad*2,0, 0)
 moon.pos=earth.pos+(moonDis, 0, 0)
 sun.pos=vector((sunDis/2),0,0)
 
 
-#Make panel to place buttons and slider:
+####Make panel to place buttons and slider:
 p = w.panel
+wx.StaticText(p,pos=(L-2*d,4), size=(L-2*d,4), label='Moon Distance')
+####Functionality of buttons and slider:
 
-#Place slider:
-s1 = wx.Slider(p, pos=(1.0*L,0.8*L), size=(0.9*L,20), minValue=0, maxValue=100)
-wx.StaticText(p, pos=(1.0*L,0.75*L), label='Set distance between Moon and Earth')
+def moveMoon(value):
+    moon.pos.x += 10000*value
+
+def setMove(evt):
+    value = s1.GetValue()
+    moveMoon(value) #value is range from 0 - 100
+
+
+####Place slider:
+wx.StaticText(p, pos=(1.0*L,0.65*L), label='Set distance between Moon and Earth')
+s1 = wx.Slider(p, pos=(1.0*L,0.7*L), size=(0.9*L,20), minValue=0, maxValue=100)
+s1.Bind(wx.EVT_SCROLL, setMove)
 
 
