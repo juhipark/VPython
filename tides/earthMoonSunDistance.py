f9a85e0952f6beee4f0c59a0d17c738b416d5b05
juhipark <juhipark@student.hartnell.edu>
2017-11-02 18:48:29 -0700

    Change descriptions fonts for display and slider, added two buttons

--- tides/earthMoonSunDistance.py
+++ tides/earthMoonSunDistance.py
@@ -5,6 +5,8 @@ import wx
 
 L=400
 Hgraph=400
+font1 = wx.Font(18, wx.DECORATIVE, wx.NORMAL, wx.BOLD)
+font2 = wx.Font(16, wx.ROMAN, wx.ITALIC, wx.NORMAL) 
 ####Create a window.
 w = window(width=2*L, height=L+window.dheight+window.menuheight+Hgraph,
            menus=True, title='Tide Change Simulation', style=wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
@@ -12,7 +14,7 @@ w = window(width=2*L, height=L+window.dheight+window.menuheight+Hgraph,
 ####Placing display
 d=20#margin
 Hdisp=200
-disp = display(window=w, x=d, y=d, width=L*2-(d*2), height=Hdisp, forward=-vector(0,1,2),
+disp = display(window=w, x=d, y=d+7, width=L*2-(d*2), height=Hdisp, forward=-vector(0,1,2),
                background=color.black)
 #########gdisplay(window=w, y=disp.height+50, width=2*(L+window.dwidth), height=Hgraph)
 
@@ -52,21 +54,41 @@ sun.pos=earth.pos+((sunDis),0,0)
 
 ####Make panel to place buttons and slider:
 p = w.panel
-wx.StaticText(p,pos=(L-2*d,4), size=(L-2*d,4), label='Moon Distance')
+displayTitle = wx.StaticText(p, pos=(L-2*d,4), label='Moon Display')
+displayTitle.SetFont(font1)
+displayTitle.SetBackgroundColour((247, 181, 158))
+
 ####Functionality of buttons and slider:
+
 
 def moveMoon(value):
     d = (sunDis-sunRad*2-earthRad*2 - moonDis)
     moon.pos.x = initialPos + d*(value/100)
-    
+
+def setLow(evt):
+    moveMoon(0)
+
+def setHigh(evt):
+    moveMoon(100)
 
 def setMove(evt):
     value = s1.GetValue()
     moveMoon(value) #value is range from 0 - 100
 
+###Place butttons:
+low = wx.Button(p, label='Low tide', pos=(0.25*L,0.7*L))
+low.Bind(wx.EVT_BUTTON, setLow)
+high = wx.Button(p, label='High tide', pos=(0.50*L,0.7*L))
+high.Bind(wx.EVT_BUTTON, setHigh)
 
 ####Place slider:
-wx.StaticText(p, pos=(1.0*L,0.65*L), label='Set distance between Moon and Earth')
+sliderTitle = wx.StaticText(p, pos=(1.0*L,0.65*L), label='Set distance between Moon and Earth')
+sliderTitle.SetFont(font1)
+sliderTitle.SetForegroundColour((134, 66, 244))
+sliderTitle.SetBackgroundColour((244, 226, 65))
+describe = wx.StaticText(p, pos=(1.0*L,0.75*L),
+              label='Slide left- Closer to Earth, Slide right- Closer to Sun')
+describe.SetFont(font2)
 s1 = wx.Slider(p, pos=(1.0*L,0.7*L), size=(0.9*L,20), minValue=0, maxValue=100)
 s1.Bind(wx.EVT_SCROLL, setMove)
 
