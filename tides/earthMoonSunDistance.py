1e10fd832a5f6c61b92175fcfb6d8c0d9d22c7f0
juhipark <juhipark@student.hartnell.edu>
2017-11-02 17:57:26 -0700

    Updated with new diameter values for earth, moon, sun

--- tides/earthMoonSunDistance.py
+++ tides/earthMoonSunDistance.py
@@ -12,7 +12,8 @@ w = window(width=2*L, height=L+window.dheight+window.menuheight+Hgraph,
 ####Placing display
 d=20#margin
 Hdisp=200
-disp = display(window=w, x=d, y=d, width=L*2-(d*2), height=Hdisp, forward=-vector(0,1,2), background=color.white)
+disp = display(window=w, x=d, y=d, width=L*2-(d*2), height=Hdisp, forward=-vector(0,1,2),
+               background=color.black)
 #########gdisplay(window=w, y=disp.height+50, width=2*(L+window.dwidth), height=Hgraph)
 
 #Mass of Earth: 5.97e21t
@@ -22,30 +23,31 @@ earthMass = 5.97e21
 moonMass = 7.36e19
 sunMass = 2e27
 
-#Mean radius of Earth: 6.37e3km
-#Mean radius of Moon: 1.74e3km
-#Mean radius of Sun: 6.95e5km
-"""Earth and Moon will be too small to see, adjustment is needed to enlarge moon and sun"""
-earthRad = 9.37e5
-moonRad = 5.74e5
-sunRad = 7.00e6
+#radius of Earth: 6.37e3km
+#radius of Moon: 1.74e3km
+#radius of Sun: 6.95e5km
+"""Adjustment is needed to make each Earth, Moon, Sun to be visible"""
+earthRad = 4.37e6
+moonRad = 6.74e5
+sunRad = 1.99e7
 
 #Distance bewtween:
-"""Too far apart, adjustment is needed"""
 #Earth to Moon: 384835km
-moonDis = 3.85e5+earthRad*2+moonRad*2
 #Earth to sun: 149785000km e8
+moonDis = 3.85e5
 sunDis = 1.50e8
 
-
-earth = sphere(material=materials.BlueMarble, radius=earthRad*2)
-moon = sphere(color=color.gray(2), material=materials.shiny, radius=moonRad*2)
-sun = sphere(pos=vector((sunDis/2),0,0), color=color.yellow, material=materials.emissive, radius=sunRad*2)
+disp.range = sunDis/2+earthRad
+disp.center = (sunDis/2,0,0)
+earth = sphere(material=materials.BlueMarble, radius=earthRad)
+moon = sphere(color=color.gray(2), material=materials.shiny, radius=moonRad)
+sun = sphere(pos=vector((sunDis/2)+sunRad,0,0), color=color.yellow, material=materials.emissive, radius=sunRad)
 
 ####starting position
-earth.pos=vector(-(sunDis/2)+earthRad*2,0, 0)
-moon.pos=earth.pos+(moonDis, 0, 0)
-sun.pos=vector((sunDis/2),0,0)
+earth.pos=vector(0,0, 0)
+initialPos = earth.pos.x+moonDis+moonRad*2+earthRad*2
+moon.pos=(initialPos, 0, 0)
+sun.pos=earth.pos+((sunDis),0,0)
 
 
 ####Make panel to place buttons and slider:
@@ -54,7 +56,9 @@ wx.StaticText(p,pos=(L-2*d,4), size=(L-2*d,4), label='Moon Distance')
 ####Functionality of buttons and slider:
 
 def moveMoon(value):
-    moon.pos.x += 10000*value
+    d = (sunDis-sunRad*2-earthRad*2 - moonDis)
+    moon.pos.x = initialPos + d*(value/100)
+    
 
 def setMove(evt):
     value = s1.GetValue()
